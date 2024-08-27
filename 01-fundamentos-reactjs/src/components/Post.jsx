a79e5cbf9393d3c33ee4a28f10a28b97498d61fc
luanhsr <luanhenriquework@gmail.com>
2024-08-26 23:15:04 -0300

    Deletando o comentario (imutabilidade)

--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -29,8 +29,11 @@ export function Post({author, publishedAt, content}) {
   function handleNewCommentChange () {
     setNewCommentText(event.target.value);
   }
-  function deleteComment (comment) {
-    console.log(`Deletar comentario ${comment}`);
+  function deleteComment (commentToDelete) {
+    const commentsWithoutDeletedOne = comments.filter(comment =>{
+      return comment !== commentToDelete;
+    }) 
+    setComments(commentsWithoutDeletedOne)
   }
   return (
     <article className={styles.post}>
@@ -77,7 +80,7 @@ export function Post({author, publishedAt, content}) {
           {comments.map(comment => {
             return (
               <Comment
-                key={content}
+                key={comment}
                 content={comment}
                 onDeleteComment={deleteComment}
               />
